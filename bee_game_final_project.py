4b78efe3fb3b6de6d29971abdc5527e9630b9cca
Xuyuan Chen <xuyuanc@andrew.cmu.edu>
2023-04-15 19:58:16 -0400

    TP1 Updates

--- bee_game_final_project.py
+++ bee_game_final_project.py
@@ -13,12 +13,13 @@ class Bee:
 
 class Flower:
     flowerList=[(300,300),(50,50),(100,100),(150,80)]
+    pollinator=[(500,200),(600,300),(550,450),(330,400)]
     def __init__(self,x,y,color,pollinator):
         self.x=x
         self.y=y
         self.color=color
         self.pollinator=True
-        self.gather=False
+        self.gathered=False
 
     def drawFlower(self):
         drawCircle(self.x,self.y,15,fill=self.color)
@@ -27,29 +28,41 @@ class Flower:
             return True
         else: 
             return False
+        
     def gatheredState(self,app): 
         if self.pollinator:
-            if not self.gather:
-                for (flowerX,flowerY) in Flower.flowerList:
+            if not self.gathered:
+                for (flowerX,flowerY) in Flower.pollinator:
                     if Flower(flowerX,flowerY,"red",None).isClose(app.player):
+                        self.gathered=True
                         return True
 
 def onAppStart(app):
+    app.width=800
+    app.height=800
     app.cursorX=200
     app.cursorY=200
     app.player=Bee(200,200)
+    app.numOfPollen=0
+    app.pollen=[]
+    app.alreadyGathered=[]
 
 def redrawAll(app):
     app.player.drawPlayer()
-    for (flowerX,flowerY) in Flower.flowerList:
+    for i in range(len(Flower.pollinator)):
+        flowerX=Flower.pollinator[i][0]
+        flowerY=Flower.pollinator[i][1]
         Flower(flowerX,flowerY,"red","pollinator").drawFlower()
-        if Flower(flowerX,flowerY,"red","pollinator").gatheredState(app): 
-            drawRect(200,200,50,50,align="center", fill="orange")
+    for (cx,cy) in app.pollen:
+        drawCircle(cx,cy,10,fill="red")
 
 def onMouseMove(app,mouseX,mouseY):
     app.cursorX=mouseX
     app.cursorY=mouseY
-
+    if Flower(0,0,"red","pollinator").gatheredState(app):
+            app.numOfPollen+=1
+            app.pollen.append((25+10*app.numOfPollen,25))
+            app.alreadyGathered.append((app.cursorX,app.cursorY))
 def onStep(app): 
     app.player.playerOnStep(app)
 
